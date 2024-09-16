2727075b9be1306f98a35989b16da1919784d862
jacopolegovini <legovini96@gmail.com>
2024-09-16 15:44:54 +0200

    header structure completed

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,7 +2,18 @@
 export default {
     data() {
         return {
-
+            menu: [
+                'Characters',
+                'Comics',
+                'Movies',
+                'TV',
+                'Games',
+                'Collectibles',
+                'Videos',
+                'Fans',
+                'News',
+                'Shop'
+            ]
         }
     }
 }
@@ -10,14 +21,38 @@ export default {
 
 <template>
     <header>
-        <h1>
-            header
-        </h1>
+        <div class="header-flex">
+            <div class="img-header">
+                <img src="../assets/img (5)/img/dc-logo.png" alt="logo DC">
+            </div>
+            <div class="navbar-header">
+                <ul class="navbar-header-flex d-flex">
+                    <li v-for="list in menu">
+                        {{ list }}
+                    </li>
+                </ul>
+            </div>
+        </div>
     </header>
 </template>
 
 <style scoped>
-    h1 {
-        color: red;
+    .header-flex {
+        padding: 20px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        .img-header {
+            flex-basis: 50%;
+            text-align: center;
+        }
+
+        .navbar-header-flex {
+            gap: 15px;
+            text-align: center;
+        }
     }
+
+
 </style>
